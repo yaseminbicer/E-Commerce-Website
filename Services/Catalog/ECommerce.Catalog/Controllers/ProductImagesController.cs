442812f4b051174187e818f378402447d34e11ea
Yasemin <yasemin.bicer@bahcesehir.edu.tr>
2024-04-06 15:50:05 +0300

    adding controller and registration

---
+++ Services/Catalog/ECommerce.Catalog/Controllers/ProductImagesController.cs
@@ -0,0 +1,50 @@
+﻿using ECommerce.Catalog.Dtos.ProductImageDtos;
+using ECommerce.Catalog.Services.ProductImageServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductImagesController:ControllerBase
+    {
+
+        private readonly IProductImageService _ProductImageService;
+
+        public ProductImagesController(IProductImageService ProductImageService)
+        {
+            _ProductImageService = ProductImageService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> ProductImageList()
+        {
+            var values = await _ProductImageService.GetAllProductImageAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductImageById(string id)
+        {
+            var values = _ProductImageService.GetByIdProductImageAsync(id);
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateProductImage(CreateProductImageDto createProductImageDto)
+        {
+            await _ProductImageService.CreateProductImageAsync(createProductImageDto);
+            return Ok("Ürün görselleri başarıyla eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductImage(string id)
+        {
+            await _ProductImageService.DeleteProductImageAsync(id);
+            return Ok("Ürün görselleri başarıyla silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductImage(UpdateProductImageDto updateProductImageDto)
+        {
+            await _ProductImageService.UpdateProductImageAsync(updateProductImageDto);
+            return Ok("Ürün görselleri başarıyla güncellendi");
+        }
+    }
+}
--- Services/Catalog/ECommerce.Catalog/Program.cs
+++ Services/Catalog/ECommerce.Catalog/Program.cs
@@ -1,6 +1,26 @@
-var builder = WebApplication.CreateBuilder(args);
+using ECommerce.Catalog.Services.CategoryServices;
+using ECommerce.Catalog.Services.ProductDetailServices;
+using ECommerce.Catalog.Services.ProductImageServices;
+using ECommerce.Catalog.Services.ProductServices;
+using ECommerce.Catalog.Settings;
+using Microsoft.Extensions.Options;
+using System.Reflection;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+ 
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductDetailService, ProductDetailService>();
+builder.Services.AddScoped<IProductImageService, ProductImageService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
