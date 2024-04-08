1405c724aa0158e0b0ab2bf92871ac0cb7c6eab8
Yasemin <yasemin.bicer@bahcesehir.edu.tr>
2024-04-09 01:30:13 +0300

    Orderin Api and MediatR registration

---
+++ Services/Order/Core/ECommerce.Order.Application/Services/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Order.Application.Services
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationService(this IServiceCollection services, IConfiguration configurariton)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ServiceRegistration).Assembly));
+
+        }
+    }
+}
--- Services/Order/Presentation/ECommerce.Order.WebApi/Program.cs
+++ Services/Order/Presentation/ECommerce.Order.WebApi/Program.cs
@@ -1,9 +1,16 @@
 using ECommerce.Order.Application.Features.CQRS.Handlers.AddressHandlers;
 using ECommerce.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using ECommerce.Order.Application.Interfaces;
+using ECommerce.Order.Application.Services;
+using ECommerce.Order.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddApplicationService(builder.Configuration);
+
+
+#region
 builder.Services.AddScoped<GetAddressQueryHandler>();
 builder.Services.AddScoped<GetAddressByIdQueryHandler>();
 builder.Services.AddScoped<CreateAddressCommandHandler>();
@@ -15,6 +22,9 @@ builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
 builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
 builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
 builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
+#endregion
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
